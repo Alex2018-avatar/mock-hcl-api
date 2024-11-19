35164d6b9eda83903fc3e7bf721e16c7d30f14f9
Alex Principe <abecerra@avatar-global.com>
2024-11-19 14:49:22 -0500

    feat: se agregan productos

--- config/products-map.js
+++ config/products-map.js
@@ -13,5 +13,40 @@ export const productsMapData = [
     id: '3074457345616817100',
     partNumber: 'PO_Equ70055665',
     jsonName: 'iphone-15-pro-max-256gb-po-equ70055665'
-  }
+  },
+  {
+    id: '3074457345617889169',
+    partNumber: 'PO_Equ70058702P',
+    jsonName: 'huawei-nova-12s-256gb-4g'
+  },
+  {
+    id: '3074457345617885668',
+    partNumber: 'PO_Equ70055299P',
+    jsonName: 'honor-90-lite-256gb-5g'
+  },
+  {
+    id: '3074457345617899176',
+    partNumber: 'PO_Equ70060041P',
+    jsonName: 'honor-x8b-512gb-4g'
+  },
+  {
+    id: '3074457345617886669',
+    partNumber: 'PO_Equ70059283P',
+    jsonName: 'motorola-moto-g04s-128gb-4g'
+  },
+  {
+    id: '3074457345617883668',
+    partNumber: 'PO_Equ70057205P',
+    jsonName: 'kalley-black-1-128gb-4g'
+  },
+  {
+    id: '3074457345617895169',
+    partNumber: 'PO_Equ70060615P',
+    jsonName: 'samsung-galaxy-z-flip-6-256gb-5g-gratis-case---buds-3'
+  },
+  {
+    id: '3074457345616817089',
+    partNumber: 'PO_Equ70055579P',
+    jsonName: 'iphone-15-128gb-5g'
+  },
 ]
--- scripts/create-products.js
+++ scripts/create-products.js
@@ -8,19 +8,52 @@ import { fileURLToPath } from 'url';
 const fsPromises = fs.promises;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+console.log('__dirname: ', __dirname);
+const storeId = '9701';
 
+const ids = [
+  '3074457345617889169', 
+  '3074457345617885668', 
+  '3074457345617899176',
+  '3074457345617886669',
+  '3074457345617883668',
+  '3074457345617895169',
+  '3074457345616817089'
+]
 
 // /mock-hcl-api/data/9701-store/search/3074457345616680171-by-category.json
 
-const readAndWriteProduct = async (storeId, categoryId) => {
+const saveData = async (widgetName, data) => {
+  const filePath = path.resolve(__dirname, `../data/${storeId}-store/product/${widgetName}.json`);
   try {
+    await fsPromises.writeFile(filePath, JSON.stringify(data, null, 2));
+    logger.info(`[SAVED] ${widgetName}`);
+    logger.info(`************************************************************`);
+  } catch (error) {
+    console.error(`Error al guardar el widget ${widgetName}:`, error);
+  }
+}
+
+const readAndWriteProduct = async (storeId, categoryId) => {
+  console.log('categoryId: ', categoryId);
+  try {
+    logger.info(`************************************************************`);
     const filePath = path.resolve(__dirname, `../data/${storeId}-store/search/${categoryId}-by-category.json`);
+    console.log('filePath: ', filePath);
+    logger.info(`************************************************************`);
     const data = await fsPromises.readFile(filePath, 'utf8');
     const response = JSON.parse(data);
-    console.log('response: ', response);
+    const { contents } = response;
+    const foundProducts = contents.filter(product => ids.includes(product.id));
+    console.log('foundProducts: ', foundProducts.length);
+    // save in file  every product found
+    foundProducts.forEach(async product => {
+      await saveData(product.seo.href ?? product.id, product.productDetail);
+    })
+    //console.log('response: ', response);
   } catch (error) {
     console.log('error: ', error);
 
   }
 };
-readAndWriteProduct(9701, 3074457345616680171);
+readAndWriteProduct(storeId, '3074457345616680171');
