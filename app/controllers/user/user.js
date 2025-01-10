084967ce789c0757d6475ff10ba7ac3a6fce568f
Alex Principe <abecerra@avatar-global.com>
2025-01-10 16:39:23 -0500

    feat: envio - validacion y consulta de telefonos en salecare

--- app/controllers/user/user.js
+++ app/controllers/user/user.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'url';
 import { logger } from '../../config/logger.js';
 import { getAddressBook } from '../../services/shipping.js';
 import { UserService } from '../../services/user.js';
+import { UserHelper } from '../../helpers/UserHelper.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,10 +13,16 @@ const fsPromises = fs.promises;
 export class UserController {
   static async getUserHandler(req, res) {
     const { _folder, userType, logged } = req;
+    const { xProcessFlow } = req.query
     const userIdFromCookie = req.cookies.USER_ID;
     const userId = req?.session?.userAuth?.userId || userIdFromCookie;
     const user = await UserService.getUserById(_folder, userId);
     const contact = await UserService.getAddressBook(_folder);
+
+    if (xProcessFlow === 'b2bShipping') {
+      const selfcareUserInfo = await UserHelper.getSelfCareUserInfo('error');
+      return res.status(200).json(selfcareUserInfo);
+    }
 
     if (logged) {
       res.status(200).json({
--- app/helpers/UserHelper.js
+++ app/helpers/UserHelper.js
@@ -1,4 +1,47 @@
 export class UserHelper {
-  static async getSelfCareUserInfo(userId) {
+  static async getSelfCareUserInfo(data = "full") {
+    if (data === "full") {
+      return {
+        "x_findPhonesSelfcare": {
+          "primaryPhone": "1111111111",
+          "code": "SUCCESS",
+          "additionalPhone": "2222222222"
+        }
+      }
+    }
+
+    if (data === "empty") {
+      return {
+        "x_findPhonesSelfcare": {
+          "code": "FETCH_USERPHONE_EMPTY"
+        },
+      }
+    }
+
+    if (data === "main") {
+      return {
+        "x_findPhonesSelfcare": {
+          "primaryPhone": "1111111111",
+          "code": "SUCCESS"
+        },
+      }
+    }
+
+    if (data === "secondary") {
+      return {
+        "x_findPhonesSelfcare": {
+          "code": "SUCCESS",
+          "additionalPhone": "2222222222"
+        },
+      }
+
+    }
+
+    return {
+      "x_findPhonesSelfcare": {
+        "code": "FETCH_USERPHONE_GENERIC_ERROR",
+        "message": "Inténtalo más tarde|Algo salió mal al procesar tu solicitud"
+      },
+    };
   }
 }
